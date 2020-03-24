add209a5f6e7bc2d2c9bb3f0bfb621aaa42dd00c
darkparis <jlmartinr1984@gmail.com>
2020-03-24 13:25:58 +0100

    Añadida arquetipo de menu con pantalla de detalle, despues de un logado exitoso

--- app/src/main/java/es/pjd/ui/login/LoginActivity.java
+++ app/src/main/java/es/pjd/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,6 +23,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import es.pjd.MenuActivity;
 import es.pjd.R;
 import es.pjd.ui.login.LoginViewModel;
 import es.pjd.ui.login.LoginViewModelFactory;
@@ -120,9 +122,11 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
+
         // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+        //Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
 
     private void showLoginFailed(@StringRes Integer errorString) {
