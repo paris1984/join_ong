3d9aabb7ac216b87ed6db8da9dda59b4eca1bd9c
darkparis <jlmartinr1984@gmail.com>
2020-03-30 10:32:56 +0200

    Nueva pantalla

---
+++ app/src/main/java/es/pjd/DispatcherActivity.java
@@ -0,0 +1,14 @@
+package es.pjd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DispatcherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dispatcher);
+    }
+}
